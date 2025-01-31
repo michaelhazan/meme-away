c702339278545010855eb15fdde35c3062b74456
rivkamiller <rivkamiller@hilma.tech>
2025-01-31 22:05:00 +0200

    feat(client): add text header

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <div>
+      <div>logo</div>
+      <div>Room Name</div>
+      <div>Profile img</div>
+    </div>
+  );
+};
---
+++ client/src/components/Layout.tsx
@@ -0,0 +1,14 @@
+// src/components/layout/Layout.tsx
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { Header } from './Header';
+
+export const Layout: React.FC = () => {
+  return (
+    <div>
+      layout
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
