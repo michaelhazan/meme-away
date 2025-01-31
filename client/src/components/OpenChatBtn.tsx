b9b92f3b1c6491c8c7f8e6d50b2a5f29d49daf80
rivkamiller <rivkamiller@hilma.tech>
2025-02-01 00:12:16 +0200

    feat(client): add open chat btn that opens a dialog

--- client/src/components/OpenChatBtn.tsx
+++ client/src/components/OpenChatBtn.tsx
@@ -1,12 +1,32 @@
+import { useState } from 'react';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import Button from '@mui/material/Button';
 import '../styles/open-chat-btn.scss';
 
 export const OpenChatBtn = () => {
-  const openChat = () => {
-    return <h1>chat opened</h1>;
-  };
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="open-chat-btn" onClick={openChat}>
-      <img src="" alt="Open group chat" />
-    </div>
+    <>
+      <div className="open-chat-btn">
+        <img
+          src="/chat-icon.svg"
+          alt="Open group chat"
+          onClick={() => setOpen(true)}
+        />
+      </div>
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>Room name Chat</DialogTitle>
+        <DialogContent>
+          here will be a list of the messages send so far
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)}>Close</Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
