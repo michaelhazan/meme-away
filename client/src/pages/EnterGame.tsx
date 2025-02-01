215b117ea613010036524be13b1bd67cf445579f
RamAvni <ramavni@hilma.tech>
2025-02-01 11:25:43 +0200

    feat(client): basic components for enterGame page, with a function that swaps between them

--- client/src/pages/EnterGame.tsx
+++ client/src/pages/EnterGame.tsx
@@ -1,3 +1,31 @@
-export const EnterGame = () => {
-  return <div>enter game</div>;
+import { FC, useState } from 'react';
+
+import { CreateRoom } from '../components/CreateRoom';
+import { JoinRoom } from '../components/JoinRoom';
+
+enum EntranceEnum {
+  JOIN = 'join',
+  CREATE = 'create',
+}
+
+export const EnterGame: FC = () => {
+  const [entrance, setEntrance] = useState<EntranceEnum>(EntranceEnum.JOIN);
+
+  function handleChangeEntrance() {
+    if (entrance === EntranceEnum.JOIN) {
+      setEntrance(EntranceEnum.CREATE);
+    } else {
+      setEntrance(EntranceEnum.JOIN);
+    }
+  }
+
+  return (
+    <div>
+      {entrance === EntranceEnum.JOIN ? (
+        <JoinRoom handleChangeEntrance={handleChangeEntrance} />
+      ) : (
+        <CreateRoom handleChangeEntrance={handleChangeEntrance} />
+      )}
+    </div>
+  );
 };
